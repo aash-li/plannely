0626e26f195618bd663eacdb2fb028cc3413d716
awliao <awliao@uw.edu>
2020-09-01 17:21:50 -0700

    added bare files

---
+++ plannely/plannely/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  plannely
+//
+//  Created by Ashley Liao on 9/1/20.
+//  Copyright © 2020 Ashley Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
