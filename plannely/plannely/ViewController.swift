d5501a8b941abb879ad92923782ea39626860b83
awliao <awliao@uw.edu>
2020-09-22 03:30:01 -0700

    added table view and background. added load screen.

--- plannely/plannely/ViewController.swift
+++ plannely/plannely/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ToDoItem: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var date: Date = Date()
+    @objc dynamic var desc: String = ""
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table: UITableView!
+    
+    private var data = [ToDoItem]()
+    private var realm = try! Realm()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        data = realm.objects(ToDoItem.self).map({ $0 })
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
